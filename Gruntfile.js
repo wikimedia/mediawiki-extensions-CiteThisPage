26d286642504584f40697797118d21badd3f256a
James D. Forrester <jforrester@wikimedia.org>
2019-05-06 15:37:39 -0700

    build: Install eslint
    Change-Id: Ia34ce1a961d4b96c9467430576777223e9cda2ff

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,27 @@
  * @package CiteThisPage
  */
 
-/*jshint node:true */
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		banana: {
-			all: ["i18n/"]
+		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true,
+				cache: true
+			},
+			all: [
+				'**/*.js',
+				'!{vendor,node_modules}/**'
+			]
 		},
+		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
 				'**/*.json',
@@ -30,6 +41,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'jsonlint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
